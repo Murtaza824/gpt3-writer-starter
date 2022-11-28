411871565a8f7b91ac2dffd54f3f79f51dca84f0
Murtaza824 <66223556+Murtaza824@users.noreply.github.com>
2022-11-28 10:41:37 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
   return (
     <div className="root">
       <div className="container">
@@ -15,6 +17,12 @@ const Home = () => {
           </div>
         </div>
         <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+           />;
           <textarea placeholder="start typing here" className="prompt-box" />
         </div>
       </div>
